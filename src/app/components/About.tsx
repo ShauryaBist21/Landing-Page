331173acee100ee52afa321e1f02a6b3382c1e28
ShauryaBist21 <shauryabist2020@gmail.com>
2024-11-30 22:45:00 +0530

    Change Intro and About us

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,16 +1,17 @@
+import React from 'react';
 import styles from './About.module.css';
 
-const About= () => {
+const About = () => {
   return (
     <div id='aboutus'> 
-    <section className={styles.aboutSection}>
-      <h2 className={styles.title}>About Zenoheal</h2>
-      <p className={styles.description}>
-        Our mission is to democratize access to advanced GAI technologies, making them accessible and beneficial for Healthcare, Education, Retail, & Digital Marketing across Asia.
-      </p>
-    </section>
+      <section className={styles.aboutSection}>
+        <h2 className={styles.title}>About Zenoheal</h2>
+        <p className={styles.description}>
+          Our mission is to enable the digital transformation of the Healthcare & Education industry by democratizing access to advanced AI technologies.
+        </p>
+      </section>
     </div>
   );  
 };
 
-export default About;
+export default About;
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import styles from './Header.module.css';
 
 const Header: React.FC = () => {
@@ -6,7 +7,7 @@ const Header: React.FC = () => {
       <div className={styles.container}>
         <h1 className={styles.title}>AI Excellence, Delivered</h1>
         <p className={styles.description}>
-          Welcome to Zenoheal, a Bayantiz Technologies AIaaS venture, dedicated to revolutionizing the way businesses leverage artificial intelligence.
+          Welcome to Zenoheal, a pioneering Generative AI-as-a-Service (AIaaS) company transforming how businesses leverage artificial intelligence. At Zenoheal, we deliver cutting-edge AI solutions that boost operational efficiency, fuel growth, and create new opportunities across diverse industries. With a commitment to innovation and excellence, we empower organizations to harness AI's full potential for a smarter, more connected future across Healthcare, Education, Retail, and Digital Marketing.
         </p>
         <div className={styles.buttonContainer}>
           <a href="#" className={styles.learnMoreButton}>Learn More</a>
