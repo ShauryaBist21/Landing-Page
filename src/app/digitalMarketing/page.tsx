56ba0562f72bca9ad14bda79a4f0e30e5eb7d9ad
ShauryaBist21 <151907652+ShauryaBist21@users.noreply.github.com>
2025-01-25 14:05:58 +0530

    Add files via upload

--- src/app/digitalMarketing/page.tsx
+++ src/app/digitalMarketing/page.tsx
@@ -2,14 +2,26 @@
 import React from "react";
 import NavBar from "../components/NAVBAR";
 import Heading from "./components/heading";
-import BusinessRedefine from "./components/BusinessRedefine";
+import Step1 from "./components/First";
+import Step2 from "./components/Second";
+import Step3 from "./components/Third";
+import Step4 from "./components/Fourth";
+import Footer from "../components/Footer";
 
 const DigitalTransformation: React.FC = () => {
   return (
     <div>
-       <NavBar/>
-       <Heading/>
-       <BusinessRedefine/>
+      <NavBar/>
+      <Heading/>
+     <Step1/>   
+     <Step2/>
+     <Step3/>
+     <Step4/>
+     <Footer/>
+     
+
+    
+     
     </div>
   );
 };
