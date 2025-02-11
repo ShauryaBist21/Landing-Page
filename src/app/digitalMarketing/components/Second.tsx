83efe4853714a76c350e7ff41602a13bfa5909f5
ShauryaBist21 <151907652+ShauryaBist21@users.noreply.github.com>
2025-02-11 23:40:15 +0530

    Add files via upload

---
+++ src/app/digitalMarketing/components/Second.tsx
@@ -0,0 +1,17 @@
+import styles from './Second.module.css';
+
+const Second = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.box}></div>
+      <div className={styles.content}>
+        <p className={styles.step}>STEP 2</p>
+        <h1 className={styles.title}>
+          Redefine the way you do business with Zenoheal Solutions
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Second;
---
+++ src/app/enterpriseSolutions/components/Third.tsx
@@ -0,0 +1,17 @@
+import styles from './Third.module.css';
+
+const First = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <p className={styles.step}>STEP 3</p>
+        <h1 className={styles.title}>
+          Redefine the way you do business with Zenoheal Solutions
+        </h1>
+      </div>
+      <div className={styles.box}></div>
+    </div>
+  );
+};
+
+export default First;
